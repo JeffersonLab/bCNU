4a475308ee81b8a859aee41a26dfa3c20437ac44
Nathan Baltzell <baltzell@gmx.com>
2025-08-18 19:44:49 -0400

    bugfix

--- mvn-install-coatjava.sh
+++ mvn-install-coatjava.sh
@@ -4,7 +4,7 @@ version=13.2.0-CED
 coatjava=$1
 
 mvn install:install-file \
-  -Dfile=$coatjava/lib/clas/coat-libs-$version-CED.jar \
+  -Dfile=$coatjava/lib/clas/coat-libs-$version.jar \
   -DgroupId=org.jlab.coat \
   -DartifactId=coat-libs \
   -Dversion=$version \
