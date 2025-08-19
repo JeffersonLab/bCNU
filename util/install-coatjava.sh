ab38068e93609b4d7927abef12634f4f9dec22ac
Nathan Baltzell <baltzell@jlab.org>
2025-08-19 16:10:14 -0400

    custom coatjava build script

---
+++ util/install-coatjava.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version=$1
+branch_or_tag=$2
+
+# abort on any non-zero exit code:
+set -e
+
+# git and build coatjava:
+git clone https://github.com/jeffersonlab/coatjava cjvbuild
+cd cjvbuild
+git checkout $branch_or_tag
+./build-coatjava.sh
+cd -
+
+# install subset of coatjava in cedbuild:
+mkdir -p cedbuild/coatjava/etc/bankdefs cedbuild/coatjava/lib/clas
+cp -r cjvbuild/coatjava/etc/bankdefs/hipo4 cedbuild/coatjava/etc/bankdefs
+cp -r cjvbuild/coatjava/lib/clas/coat-libs-$version.jar cedbuild/coatjava/lib/clas
+rm -rf cjvbuild
+
